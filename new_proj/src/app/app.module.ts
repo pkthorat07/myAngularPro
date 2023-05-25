653dc359b1871543636b4864142ee7cd97c434e6
pkthorat07 <pavankumarthorat4@gmail.com>
2023-05-26 01:26:58 +0530

    pro is done

--- new_proj/src/app/app.module.ts
+++ new_proj/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
